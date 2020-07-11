4e04eb59f28d90aa01cd92c530ce30889cc67be3
Marmik2003 <44599895+Marmik2003@users.noreply.github.com>
2020-07-11 14:40:43 +0530

    Update botmind.py

--- botmind.py
+++ botmind.py
@@ -23,3 +23,30 @@ def chat_reply(msg):
         unkn_inpt_dict = {'input':msg,'Date & Time':now}
         garbage_collection.insert_one(unkn_inpt_dict)
         return 'Sorry, I don\'t know about that. But, I stored it in my database so that my developers can review that!'
+import pymongo
+from pymongo import MongoClient
+from random import choice
+from datetime import datetime
+
+now = datetime.now()
+
+cluster = MongoClient("mongodb+srv://root:1234@careerbot.i0gdj.mongodb.net/<dbname>?retryWrites=true&w=majority")
+db = cluster["res_data"]
+collection = db["res_data"]
+
+def chat_reply(msg):
+    inpt='"'+msg+'"'
+    query = {'input':inpt}
+    try:
+        doc = collection.find(query)
+        output_list =[]
+        for message in doc:
+            output_list.append(message)
+        random_msg = choice(output_list)
+
+        return random_msg['output']
+    except IndexError:
+        garbage_collection = db["unknown_inputs"]
+        unkn_inpt_dict = {'input':msg,'Date & Time':now}
+        garbage_collection.insert_one(unkn_inpt_dict)
+        return 'Sorry, I don\'t know about that. But, I stored it in my database so that my developers can review that!'
